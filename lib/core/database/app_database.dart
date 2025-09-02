96bcfe1448a879de72b587baa776f126fb0bc15b
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-02 14:06:22 -0300

    FEAT: AppDatabase class basic methods (getDatabase and closeDatabase)

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,11 +1,37 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:ticketsales/core/errors/opening_database_error.dart';
 
-Future<Database> getDatabase() async {
-  final String? database_url = dotenv.get("DATABASE_URL");
-  if(database_url == null)
-  {
-    
+class AppDatabase {
+  Database? db_instance;
+
+  AppDatabase();
+
+  Future<void> getDatabase() async {
+    final String? database_url = dotenv.get("DATABASE_URL");
+    if(database_url == null)
+    {
+      throw new OpeningDatabaseError(".env file needs 'DATABASE_URL' field");
+    }
+
+    if(db_instance != null){
+      throw new OpeningDatabaseError("Database is already open");
+    }
+    db_instance = await openDatabase(database_url);
+
   }
-  final Database db = await openDatabase("");
+
+  Future<void> closeDatabase() async {
+    final String? database_url = dotenv.get("DATABASE_URL");
+    if(database_url == null)
+    {
+      throw new OpeningDatabaseError(".env file needs 'DATABASE_URL' field");
+    }
+    if(db_instance == null)
+    {
+      throw new OpeningDatabaseError("Database is already closed");
+    }
+    await db_instance!.close();
+    }
+
 }
